51560e623dae73ba0dc0fa1b46180c25eff41fd8
zakky <sadaoza@gmail.com>
2024-06-20 08:59:38 +0900

    Link Componet

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function FirstPost() {
   return (
     <div>
       <h1>最初の投稿</h1>
       <p>こんにちは</p>
+      <Link href="/"><a>ホームへ戻る</a></Link>
     </div>  
   );
 }
-
