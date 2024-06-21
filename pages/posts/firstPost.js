27aaa2f9c9dc52f1077713342db0cb727d2547c2
zakky <sadaoza@gmail.com>
2024-06-21 21:04:28 +0900

    Image&Head Componet

--- pages/posts/firstPost.js
+++ pages/posts/firstPost.js
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function FirstPost() {
   return (
     <div>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
       <h1>最初の投稿</h1>
       <p>こんにちは</p>
       <Link href="/"><a>ホームへ戻る</a></Link>
+      <Image src="/vercel.svg" alt="Vercel Logo" width={200} height={200} />
     </div>  
   );
 }
