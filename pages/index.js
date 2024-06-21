bab102244a87cf2d550b7d8fbdad57d4da899d2b
zakky <sadaoza@gmail.com>
2024-06-22 08:46:10 +0900

    toppage layout updates

--- pages/index.js
+++ pages/index.js
@@ -36,7 +36,7 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          私はフルスタックエンジニアです/Udemy講師として活動しています/好きな言語はJavascriptです
+          私はフルスタックエンジニアです/Shopifyエンジニアとして活動しています/好きな言語はGASとJavascriptです
         </p>
         {/* <Link href="/posts/first-post">最初の投稿はこちら</Link>  あとで外す*/}
       </section>
